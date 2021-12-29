60d771449fe606cb3b4aad6d116e537cf34f00fd
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:50:25 +0300

    Add files via upload

---
+++ lab6/LogBuffer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Timers;
+
+namespace lab6
+{
+    public class LogBuffer
+    {
+        private ConcurrentQueue<string> Words { get; } = new();
+        private readonly StreamWriter _streamWriter;
+
+        private const int Capacity = 10;
+        private const int Limit = 10000;
+
+        public LogBuffer(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("File doesn't exists: " + filePath);
+            }
+
+            _streamWriter = new StreamWriter(filePath, true);
+
+            var timer = new Timer(Limit);
+            timer.Elapsed += checkTimer;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        public void Add(string item)
+        {
+            Words.Enqueue(item);
+            CheckCapacity();
+        }
+        
+        private void checkTimer(object source, ElapsedEventArgs e)
+                {
+                    while (!Words.IsEmpty)
+                    {
+                        Words.TryDequeue(out var message);
+                        if (message != null)
+                        {
+                            _streamWriter.WriteLineAsync(message);
+                        }
+                    }
+                }
+        private void CheckCapacity()
+        {
+            if (Words.Count < Capacity)
+            {
+                return;
+            }
+            while (!Words.IsEmpty)
+            {
+                Words.TryDequeue(out string message);
+                if (message != null)
+                {
+                    _streamWriter.WriteLineAsync(message);
+                }
+            }
+        }
+
+
+        public void Close()
+        {
+            _streamWriter.Close();
+        }
+    }
+}
---
+++ lab6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logBuffer = new LogBuffer("D:\\buff.txt");
+                        for (var i = 1; i <= 50; i++)
+                        {
+                            logBuffer.Add(i.ToString());
+                        }
+                        Console.WriteLine("Press the Enter key to exit ... ");
+                        Console.ReadLine();
+                        logBuffer.Close();
+        }
+    }
+}
